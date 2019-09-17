763ec01fa984848f6c9c7ea64a8ff78da5567950
Steve Nebes <snebes@gmail.com>
2019-09-17 16:15:39 -0500

    Conditional services

--- src/DependencyInjection/SNNotificationsExtension.php
+++ src/DependencyInjection/SNNotificationsExtension.php
@@ -10,8 +10,15 @@ declare(strict_types=1);
 
 namespace SN\Bundle\NotificationsBundle\DependencyInjection;
 
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use SN\Notifications\Channel\DatabaseChannel;
+use SN\Notifications\Channel\MailChannel;
+use SN\Notifications\Contracts\MailerInterface;
+use SN\Notifications\Mailer\SwiftMailerMailer;
+use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
@@ -30,5 +37,35 @@ class SNNotificationsExtension extends Extension
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
+
+        if (\class_exists(DoctrineBundle::class)) {
+            $def = new Definition(DatabaseChannel::class, [
+                '@?doctrine.orm.entity_manager',
+                'SN\Bundle\NotificationsBundle\Entity\Notification'
+            ]);
+            $def->setPublic(false);
+            $def->addTag('kernel.event_subscriber');
+            $container->setDefinition('sn_notifications.channel.database_channel', $def);
+        }
+
+        if (\class_exists(SwiftmailerBundle::class)) {
+            // Mailer.
+            $def = new Definition(SwiftMailerMailer::class, [
+                '@?swiftmailer.mailer',
+            ]);
+            $def->setPublic(false);
+
+            $container->setDefinition('sn_notifications.mailer.swiftmailer_mailer', $def);
+            $container->setAlias(MailerInterface::class, 'sn_notifications.mailer.swiftmailer_mailer');
+
+            // Mail channel.
+            $def = new Definition(MailChannel::class, [
+                '@' . MailerInterface::class,
+            ]);
+            $def->setPublic(false);
+            $def->addTag('kernel.event_subscriber');
+
+            $container->setDefinition('sn_notifications.channel.mail_channel', $def);
+        }
     }
 }
